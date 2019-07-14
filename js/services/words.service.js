a3aea43d5905b8d79fccdee22337847444e22439
Christian Bravo <christian151.bravo@gmail.com>
2019-07-13 19:59:24 -0600

    change API url again

--- js/services/words.service.js
+++ js/services/words.service.js
@@ -2,7 +2,7 @@ angular
     .module('HangmanApp')
     .factory('wordService', ['$http', function($http) {
 
-        const wordAPI = 'https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1';
+        const wordAPI = 'https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=verb&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1';
         const wordAPI_KEY = '&api_key=09f92f4b1c190483034320ea5100bfe5068b46b9f9a8702dd';
             
         return $http({
